3389c079de32aca02c65a53026152db8a2368580
Siqi Chen <c756738098@gmail.com>
2024-12-19 09:06:52 -0600

    finish

--- src/slider.js
+++ src/slider.js
@@ -22,9 +22,7 @@ export default function Slider() {
     const [background, setBackground] = useState(
         'linear-gradient(to top, rgba(20, 20, 27, 1), rgba(20, 20, 27, 1))',
     );
-    const [backgroundStroke, setBackgroundStroke] = useState(
-        'linear-gradient(90deg, #ff0000, #ff7f00)',
-    );
+    const [backgroundStroke, setBackgroundStroke] = useState('borderBox');
     const [outsideGradientColor, setOutsideGradientColor] = useState('url(#gradient-orange)');
     const [outsideFilter, setOutsideFilter] = useState(
         'drop-shadow(0px 0px 4px rgba(252, 144, 51, 0.36))',
@@ -49,9 +47,7 @@ export default function Slider() {
             setOutsideGradientColor('url(#gradient-orange)');
             setOutsideFilter('drop-shadow(0px 0px 4px rgba(252, 144, 51, 0.36))');
             setOutsideStroke('url(#stroke-gradient)');
-            setBackgroundStroke(
-                'linear-gradient(90deg, rgba(255, 0, 0, 0.5) 0%, rgba(255, 69, 0, 0.7) 50%, rgba(255, 140, 0, 0.5) 100% )',
-            );
+            setBackgroundStroke('borderBox');
         } else if (x.get() > 0) {
             //green area
             setArrowColor('green');
@@ -62,7 +58,7 @@ export default function Slider() {
             setOutsideGradientColor('rgba(0, 0, 0, 0.4)');
             setOutsideFilter('rgba(0, 0, 0, 0.4)');
             setOutsideStroke('rgba(0, 0, 0, 0.4)');
-            setBackgroundStroke('linear-gradient(90deg, #ff0000, #ff7f00)');
+            setBackgroundStroke('redStroke');
         } else if (x.get() < 0) {
             //red area
             setArrowColor('red');
@@ -73,7 +69,7 @@ export default function Slider() {
             setOutsideGradientColor('rgba(0, 0, 0, 0.4)');
             setOutsideFilter('rgba(0, 0, 0, 0.4)');
             setOutsideStroke('rgba(0, 0, 0, 0.4)');
-            setBackgroundStroke('linear-gradient(90deg, #ff0000, #ff7f00)');
+            setBackgroundStroke('greenStroke');
         } else {
             // unexpected case
             console.warn('Unexpected value:', x.get());
@@ -106,129 +102,143 @@ export default function Slider() {
 
     return (
         <div>
-            <motion.div
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    flex: 1,
-                    width: 335,
-                    height: 65,
-                    maxWidth: '100%',
-                    borderRadius: 20,
-                    background: background,
-                    position: 'relative',
-                    overflow: 'hidden',
-                }}
-                className="borderBox"
-            >
+            <motion.div className={backgroundStroke}>
                 <motion.div
-                    style={{ ...box, x }}
-                    drag="x"
-                    dragConstraints={{ left: 0, right: 0 }}
-                    onDragEnd={handleDragEnd}
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        flex: 1,
+                        width: 335,
+                        height: 65,
+                        maxWidth: '100%',
+                        borderRadius: 20,
+                        background: background,
+                        position: 'relative',
+                        overflow: 'hidden',
+                    }}
                 >
-                    <svg className="progress-icon cursor-pointer" viewBox="0 0 50 50">
-                        {/* outside circle */}
-                        <defs>
-                            {/* orange Gradient */}
-                            <linearGradient
-                                id="gradient-orange"
-                                x1="0%"
-                                y1="0%"
-                                x2="100%"
-                                y2="100%"
-                            >
-                                <stop offset="0%" stopColor="rgba(251, 72, 51, 1)" />
-                                <stop offset="100%" stopColor="rgba(252, 159, 50, 1)" />
-                            </linearGradient>
-                            {/* stroke Gradient */}
-                            <linearGradient
-                                id="stroke-gradient"
-                                x1="0%"
-                                y1="0%"
-                                x2="100%"
-                                y2="100%"
-                                gradientUnits="userSpaceOnUse"
-                            >
-                                <stop offset="0%" stop-color="#FFFF00" />
-                                <stop offset="50%" stop-color="#FF0000" />
-                                <stop offset="100%" stop-color="#FFFF00" />
-                            </linearGradient>
-                        </defs>
+                    {' '}
+                    <motion.div
+                        style={{ ...box, x }}
+                        drag="x"
+                        dragConstraints={{ left: 0, right: 0 }}
+                        onDragEnd={handleDragEnd}
+                    >
+                        <svg className="progress-icon cursor-pointer" viewBox="0 0 50 50">
+                            {/* outside circle */}
+                            <defs>
+                                {/* orange Gradient */}
+                                <linearGradient
+                                    id="gradient-orange"
+                                    x1="0%"
+                                    y1="0%"
+                                    x2="100%"
+                                    y2="100%"
+                                >
+                                    <stop offset="0%" stopColor="rgba(251, 72, 51, 1)" />
+                                    <stop offset="100%" stopColor="rgba(252, 159, 50, 1)" />
+                                </linearGradient>
+                                {/* stroke Gradient */}
+                                <linearGradient
+                                    id="stroke-gradient"
+                                    x1="0%"
+                                    y1="0%"
+                                    x2="100%"
+                                    y2="100%"
+                                    gradientUnits="userSpaceOnUse"
+                                >
+                                    <stop offset="0%" stop-color="#FFFF00" />
+                                    <stop offset="50%" stop-color="#FF0000" />
+                                    <stop offset="100%" stop-color="#FFFF00" />
+                                </linearGradient>
+                            </defs>
 
-                        <motion.path
-                            strokeWidth="1"
-                            stroke={outsideStroke}
-                            filter={outsideFilter}
-                            fill={outsideGradientColor}
-                            d="M 0, 20 a 20, 20 0 1,0 40,0 a 20, 20 0 1,0 -40,0"
-                            style={{
-                                x: 5,
-                                y: 5,
-                            }}
-                        />
+                            <motion.path
+                                strokeWidth="1"
+                                stroke={outsideStroke}
+                                filter={outsideFilter}
+                                fill={outsideGradientColor}
+                                d="M 0, 20 a 20, 20 0 1,0 40,0 a 20, 20 0 1,0 -40,0"
+                                style={{
+                                    x: 5,
+                                    y: 5,
+                                }}
+                            />
 
-                        {/* inside circle */}
-                        <motion.path
-                            fill="rgba(37, 37, 47, 1)"
-                            strokeWidth="2"
-                            d="M 8, 20 a 12, 12 0 1,0 24,0 a 12, 12 0 1,0 -24,0"
-                            style={{
-                                x: 5,
-                                y: 5,
-                            }}
-                        />
-                    </svg>
-                </motion.div>
-
-                {/* cross and decline */}
-                <motion.div
-                    className="absolute left-4 text-sm"
-                    style={{ color: textColor, opacity: useTransform(x, [-100, 100], [1, 1]) }}
-                >
-                    {arrowColor === 'default' ? (
-                        <div className="leftLayout">
-                            <img src={whiteclose} alt="cross" /> Decline
-                            <Lottie animationData={leftAni} />{' '}
-                        </div>
-                    ) : arrowColor === 'green' ? (
-                        <div className="leftLayout">
-                            <img src={greenclose} alt="cross" /> Decline{' '}
-                            <img src={greenleft} alt="green arrow" className="w-[54px] h-8" />
-                        </div>
-                    ) : (
-                        <div className="leftLayout">
-                            <img src={redclose} alt="cross" /> Decline{' '}
-                            <img src={redleft} alt="red arrow" className="w-[54px] h-8" />
-                        </div>
-                    )}
-                </motion.div>
-
-                {/* check and accept */}
-                <motion.div
-                    className="absolute right-4 text-sm "
-                    style={{ color: textColor, opacity: useTransform(x, [-100, 100], [1, 1]) }}
-                >
-                    {arrowColor === 'default' ? (
-                        <div className="rightLayout">
-                            <Lottie animationData={rightAni} />
-                            Accept
-                            <img src={whitecheck} alt="cross" />
-                        </div>
-                    ) : arrowColor === 'green' ? (
-                        <div className="rightLayout">
-                            <img src={greenright} alt="green arrow" className="w-[54px] h-8" />
-                            Accept
-                            <img src={greencheck} alt="cross" />
-                        </div>
-                    ) : (
-                        <div className="rightLayout">
-                            <img src={redright} alt="red arrow" className="w-[54px] h-8" />
-                            Accept
-                            <img src={redcheck} alt="cross" />
-                        </div>
-                    )}
+                            {/* inside circle */}
+                            <motion.path
+                                fill="rgba(37, 37, 47, 1)"
+                                strokeWidth="2"
+                                d="M 8, 20 a 12, 12 0 1,0 24,0 a 12, 12 0 1,0 -24,0"
+                                style={{
+                                    x: 5,
+                                    y: 5,
+                                }}
+                            />
+                        </svg>
+                    </motion.div>
+                    {/* cross and decline */}
+                    <motion.div
+                        className="absolute left-4 text-sm"
+                        style={{
+                            color: textColor,
+                            opacity: useTransform(x, [-100, 100], [1, 1]),
+                        }}
+                    >
+                        {arrowColor === 'default' ? (
+                            <div className="leftLayout">
+                                <img src={whiteclose} alt="cross" /> Decline
+                                <Lottie animationData={leftAni} />{' '}
+                            </div>
+                        ) : arrowColor === 'green' ? (
+                            <div className="leftLayout">
+                                <img src={greenclose} alt="cross" /> Decline{' '}
+                                <img
+                                    src={greenleft}
+                                    alt="green arrow"
+                                    className="w-[54px] h-8"
+                                />
+                            </div>
+                        ) : (
+                            <div className="leftLayout">
+                                <img src={redclose} alt="cross" /> Decline{' '}
+                                <img src={redleft} alt="red arrow" className="w-[54px] h-8" />
+                            </div>
+                        )}
+                    </motion.div>
+                    {/* check and accept */}
+                    <motion.div
+                        className="absolute right-4 text-sm "
+                        style={{
+                            color: textColor,
+                            opacity: useTransform(x, [-100, 100], [1, 1]),
+                        }}
+                    >
+                        {arrowColor === 'default' ? (
+                            <div className="rightLayout">
+                                <Lottie animationData={rightAni} />
+                                Accept
+                                <img src={whitecheck} alt="cross" />
+                            </div>
+                        ) : arrowColor === 'green' ? (
+                            <div className="rightLayout">
+                                <img
+                                    src={greenright}
+                                    alt="green arrow"
+                                    className="w-[54px] h-8"
+                                />
+                                Accept
+                                <img src={greencheck} alt="cross" />
+                            </div>
+                        ) : (
+                            <div className="rightLayout">
+                                <img src={redright} alt="red arrow" className="w-[54px] h-8" />
+                                Accept
+                                <img src={redcheck} alt="cross" />
+                            </div>
+                        )}
+                    </motion.div>
                 </motion.div>
             </motion.div>
             <ToastContainer />
